d31045fb17d9399da00e98667eba2241f363a346
Muhammad Danish Azeem <88603558+mda1458@users.noreply.github.com>
2023-07-27 18:11:59 +0500

    chat styling

--- src/Components/Chats/Chats.jsx
+++ src/Components/Chats/Chats.jsx
@@ -3,7 +3,6 @@ import { useCollectionData } from 'react-firebase-hooks/firestore'
 import ChatMessage from './ChatMessage'
 import firebase from 'firebase/compat/app'
 import { useRef } from 'react'
-import "./chats.css"
 
 const Chats = () => {
   const messageRef = firestore.collection('messages')
@@ -25,21 +24,21 @@ const Chats = () => {
   }
 
   return (
-    <>
+    <div className="pt-20">
       {/* Chats */}
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+      <main className="flex flex-col items-center justify-center w-full flex-1 lg:px-20 text-center">
         <div className="flex flex-col w-full h-full overflow-y-auto">
           {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
           <div ref={dummy}></div>
         </div>
       </main>
       {/* Chat Input */}
-      <form className="flex items-center justify-center w-full h-24 px-4 py-2 border-t border-gray-200">
+      <form className="flex items-center justify-center w-full h-24 px-4 py-2 border-t border-gray-200" onSubmit={sendMessage}>
         <input className="flex-1 px-4 py-2 mr-4 text-xl border border-gray-300 rounded-md focus:outline-none" type="text" name="text" placeholder="Type your message here..." />
-        <button className="px-4 py-2 text-xl font-semibold text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none" onClick={sendMessage}>🕊️</button>
+        <button className="px-4 py-2 text-xl font-semibold text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none" type='submit'>🕊️</button>
       </form>
 
-    </>
+    </div>
   );
 }
 
